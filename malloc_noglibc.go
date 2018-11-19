b3a84923c6a952a61c1cf918de311f1668e8f277
zRedShift <stormrage2007@gmail.com>
2018-11-19 23:17:42 +0200

    Add "noglibc" build tag, for other (e.g. musl) libc implementations.

--- malloc_noglibc.go
+++ malloc_noglibc.go
@@ -1,9 +1,9 @@
-// +build !noglibc
+// +build noglibc
 
 package thumbnailer
 
 // #cgo CFLAGS: -std=c11
-// #include "malloc.h"
+// #include "stdlib.h"
 import "C"
 import "unsafe"
 
@@ -12,26 +12,26 @@ type MalloptParam int
 
 // All possible values of MalloptParam.
 const (
-	ArenaMax      MalloptParam = C.M_ARENA_MAX
-	ArenaTest     MalloptParam = C.M_ARENA_TEST
-	CheckAction   MalloptParam = C.M_CHECK_ACTION
-	MMapMax       MalloptParam = C.M_MMAP_MAX
-	MMapThreshold MalloptParam = C.M_MMAP_THRESHOLD
-	MXFast        MalloptParam = C.M_MXFAST
-	Perturb       MalloptParam = C.M_PERTURB
-	TopPad        MalloptParam = C.M_TOP_PAD
-	TrimThreshold MalloptParam = C.M_TRIM_THRESHOLD
+	ArenaMax      MalloptParam = 0
+	ArenaTest     MalloptParam = 0
+	CheckAction   MalloptParam = 0
+	MMapMax       MalloptParam = 0
+	MMapThreshold MalloptParam = 0
+	MXFast        MalloptParam = 0
+	Perturb       MalloptParam = 0
+	TopPad        MalloptParam = 0
+	TrimThreshold MalloptParam = 0
 )
 
 // MallocTrim calls glibc's malloc_trim. Go refuses to return freed C memory to the OS otherwise for some reason.
 func MallocTrim(pad int) bool {
-	return C.malloc_trim(C.size_t(pad)) > 0
+	return true
 }
 
 // Mallopt calls glibc's mallopt. Reducing M_TOP_PAD or M_TRIM_THRESHOLD, increasing M_MMAP_THRESHOLD, and setting
 // M_ARENA_MAX to GOMAXPROCS (it defaults to 8 * #threads), has shown good results with regards to freed memory "leaks".
 func Mallopt(param MalloptParam, value int) bool {
-	return C.mallopt(C.int(param), C.int(value)) > 0
+	return true
 }
 
 func malloc(size int) unsafe.Pointer {
